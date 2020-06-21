776d6ea0869b5047391c960aa1d20c7ff3526d4d
Adria <Adrialeogar@gmail.com>
2020-06-21 17:57:36 +0200

    Task 2

--- basics-assignment-2-start/src/app/app.module.ts
+++ basics-assignment-2-start/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SecondAssignmentComponent } from './second-assignment/second-assignment.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SecondAssignmentComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
