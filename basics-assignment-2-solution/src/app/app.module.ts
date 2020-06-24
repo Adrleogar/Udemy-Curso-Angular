7e4dc27cbaf0fecf805f26cbc13f35d4816d56db
Adria <Adrialeogar@gmail.com>
2020-06-24 12:16:44 +0200

    Task 2 done

--- basics-assignment-2-solution/src/app/app.module.ts
+++ basics-assignment-2-solution/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { SecondAssignmentComponent } from './second-assignment/second-assignment.component';
 
 @NgModule({
-  declarations: [AppComponent, SecondAssignmentComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
